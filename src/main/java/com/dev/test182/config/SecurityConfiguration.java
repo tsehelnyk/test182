0e8aba940527adcf6979518f6451afe4e0724397
tsehelnyk <58398447+tsehelnyk@users.noreply.github.com>
2020-03-18 11:11:03 +0200

    Revert "security and registration completed"

--- src/main/java/com/dev/test182/config/SecurityConfiguration.java
+++ src/main/java/com/dev/test182/config/SecurityConfiguration.java
@@ -1,39 +1,18 @@
 package com.dev.test182.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("userDetailsServiceImpl")
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder)
-            throws Exception {
-        authenticationManagerBuilder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(getEncoder());
-    }
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/registration", "/login", "/index").permitAll()
-                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                .authorizeRequests().antMatchers("/register", "/login", "/index").permitAll()
                 .antMatchers("/h2-console/**").hasRole("ADMIN")
+                .antMatchers("/data/**").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -41,12 +20,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .httpBasic();
+        httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
-    }
-
-    @Bean
-    public PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
 
--- src/main/java/com/dev/test182/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.dev.test182.service;
-
-import com.dev.test182.model.Role;
-
-public interface RoleService {
-    Role save(Role role);
-
-    Role getByName(String roleName);
-}
